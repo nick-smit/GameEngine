e7fd59ca0e1780e0799f1525808b0bdd1b0a74a6
Nick Smit <nicksmit1994@gmail.com>
2020-08-03 16:29:06 +0200

    Use MyApplication for now

--- Engine/src/GameEngine.cpp
+++ Engine/src/GameEngine.cpp
@@ -2,7 +2,17 @@
 
 #include "Core\Application.h"
 
+class MyApplication : public Engine::Application
+{
+public: 
+	MyApplication(std::string name) : Engine::Application(name) {
+		std::cout << "From MyApplication: " << this->GetName() << std::endl;
+	};
+	
+	~MyApplication() = default;
+};
+
 Engine::Application* Engine::CreateApplication()
 {
-	return new Application("Game engine!");
+	return new MyApplication("Game engine!");
 }
