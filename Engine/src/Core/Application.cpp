f59100c4738779b463fc594f5285d94e98ae1412
Nick Smit <nicksmit1994@gmail.com>
2020-08-05 11:40:28 +0200

    Implement a basic window

--- Engine/src/Core/Application.cpp
+++ Engine/src/Core/Application.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "Application.h"
 
+#include "GLFW\glfw3.h"
+
 namespace Engine {
 	Application* Application::s_Instance = nullptr;
 
@@ -11,6 +13,9 @@ namespace Engine {
 		LOG_CORE_INFO("Initializing engine v0.0.1")
 
 		s_Instance = this;
+
+		
+
 	}
 
 	Application::~Application()
@@ -20,9 +25,32 @@ namespace Engine {
 	
 	void Application::Run()
 	{
-		while (m_Running) {
-			// do stuff
+		auto success = glfwInit();
+		GE_CORE_ASSERT(success, "Failed to initialize GLFW")
+
+		/* Create a windowed mode window and its OpenGL context */
+		GLFWwindow* window = glfwCreateWindow(640, 480, m_Name.c_str(), NULL, NULL);
+		if (!window) {
+			GE_CORE_ASSERT(window, "Failed to create a window")
+
+			glfwTerminate();
+			return;
 		}
+
+		/* Make the window's context current */
+		glfwMakeContextCurrent(window);
+
+		while (!glfwWindowShouldClose(window)) {
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+
+		glfwTerminate();
+
 
 		#ifdef GE_DEBUG
 			// Wait for user input before closing the console window.
